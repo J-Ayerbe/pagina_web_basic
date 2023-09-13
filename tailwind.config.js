0b12de80819a8baed4455da66aaf071ee6944cfe
Zheone9 <jorgito.2703@gmail.com>
2023-09-13 10:20:21 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/*.html"],
+  important: true,
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "dark-blue": "#12122D",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
